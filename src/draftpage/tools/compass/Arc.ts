469392d5931dd1537ac976c2a0985890975763e8
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2018-11-09 03:56:06 +0300

    Поправил инструмент циркуль

--- src/draftpage/tools/compass/Arc.ts
+++ src/draftpage/tools/compass/Arc.ts
@@ -10,7 +10,8 @@ class Arc implements IShape {
         this._startAngle = angleStartVec.angle();
         this._radius = angleStartVec.radius();
         const angleEndVec = new Vec2(arcEndPoint.x - this._center.x, arcEndPoint.y - this._center.y);
-        this._angle = normalizeAngle(angleEndVec.angle() - this._startAngle);
+        this._angle = angleEndVec.angle() - this._startAngle;
+        this._angle = this._angle < 0 ? this._angle + Math.PI * 2 : this._angle;
     }
 
     public center(): Vec2 {
@@ -30,9 +31,21 @@ class Arc implements IShape {
     }
 
     public setArcEndVec(vec: Vec2): void {
-        let endAngle = vec.angle();
-        endAngle = endAngle == 0 ? Math.PI * 2 : endAngle;
-        this._angle = endAngle - this._startAngle;
+        if (vec.angle() < this._startAngle + this._angle && vec.angle() > this._startAngle
+            || this._startAngle > normalizeAngle(this._startAngle + this._angle) && vec.angle() < normalizeAngle(this._startAngle + this._angle)) {
+            return;
+        }
+        let startAngleDelta = this._startAngle - vec.angle();
+        startAngleDelta = startAngleDelta < 0 ? startAngleDelta + Math.PI * 2 : startAngleDelta;
+        let endAngleDelta = vec.angle() - (this._startAngle + this._angle);
+        endAngleDelta = endAngleDelta < 0 ? endAngleDelta + Math.PI * 2 : endAngleDelta;
+        if (startAngleDelta < endAngleDelta) {
+            this._startAngle = normalizeAngle(this._startAngle - startAngleDelta);
+            this._angle = Math.max(this._angle, this._angle + startAngleDelta);
+        }
+        else {
+            this._angle = Math.max(this._angle, this._angle + endAngleDelta);
+        }
     }
 
     public draw(drawingContext: IDrawingContext) {
