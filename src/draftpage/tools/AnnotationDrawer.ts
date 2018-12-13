06a871f93f536e10d4d121f6a0177b331000a6e2
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2018-12-13 09:33:17 +0300

    Добавил реальные примеры уровня. Поправил стили.

--- src/draftpage/tools/AnnotationDrawer.ts
+++ src/draftpage/tools/AnnotationDrawer.ts
@@ -5,18 +5,23 @@ import {TextAlign} from "../workplace/drawingcontext/TextAlign";
 
 const FONT = "normal 24px Helvetica";
 const TEXT_FILL = "#343434";
+const ZOOM_SCALE = 0.25;
 
 class AnnotationDrawer {
     static drawLineAnnotation(context: IDrawingContext, line: Line) {
         const textTranslate = 25;
         const dirVector = AnnotationDrawer._getLineDir(line);
-        const length = Math.floor(dirVector.radius());
+        const length = AnnotationDrawer._getLineLength(dirVector);
         dirVector.scale(textTranslate / dirVector.radius());
         const textPosition = line.end().add(dirVector);
         context.setFont(FONT);
         context.setFill(TEXT_FILL);
         context.setTextAlign(TextAlign.center);
         context.text(length.toString(), textPosition)
+    }
+
+    private static _getLineLength(line: Vec2): number {
+        return Math.floor(line.radius() * ZOOM_SCALE);
     }
 
     private static _getLineDir(line: Line): Vec2 {
