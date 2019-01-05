9ebe60813a3efaea48068415a32e0f79cd0dd50c
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-01-05 18:05:54 +0300

    Доработал сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
-const uglify = require("gulp-uglify");
-const scss = require("gulp-scss");
+const gutil = require("gulp-util");
+const sass = require("gulp-sass");
 const base64Inline = require("gulp-base64-inline");
 const browserify = require("browserify");
 const source = require('vinyl-source-stream');
@@ -11,50 +11,54 @@ const buffer = require('vinyl-buffer');
 const path = {
 	out: "bin/build",
 	tsEntryPoint: "src/app.ts",
+	scssFiles: "res/styles/**/*.scss",
 	scssEntryPoint: "res/styles/styles.scss",
 };
 
 const config = {
-	scss: {
-		noCache: true,
-		tmpPath: "",
-		style: "compressed"
-	},
-	babel: {
-		presets: ["@babel/env"],
+	sass: (debug)=> ({
+		outputStyle: !debug ? "compressed" : undefined,
+	}),
+	babel: (debug) => ({
+		presets: !debug ? ["@babel/env"] : [],
 		extensions: ['.ts'],
-		minified: true,
-	},
-	browserify: {
-		debug: false,
+		minified: !debug,
+	}),
+	browserify: (debug) => ({
+		debug,
 		entries: [path.tsEntryPoint],
 		extensions: ['.babel'],
 		cache: {},
 		packageCache: {},
-	}
+	})
 };
 
 gulp.task("compile-ts", () => {
-	return browserify(config.browserify)
+	return browserify(config.browserify(gutil.env.debug))
 		.plugin(tsify)
 		.transform("brfs")
-		.transform("babelify", config.babel)
+		.transform("babelify", config.babel(gutil.env.debug))
 		.bundle()
 		.on('error', (error) => {
 			console.log(error.message);
 		})
 		.pipe(source('index.js'))
 		.pipe(buffer())
-		.pipe(uglify())
 		.pipe(gulp.dest(path.out));
 });
 
 gulp.task("compile-scss", () => {
 	gulp.src(path.scssEntryPoint)
-		.pipe(scss(config.scss))
+		.pipe(sass(config.sass(gutil.env.debug)))
 		.pipe(base64Inline())
 		.pipe(gulp.dest(path.out));
 });
 
+gulp.task("watch-scss", ["compile-scss"], () => {
+	gutil.env.debug = true;
+	gulp.watch(path.scssFiles, ["compile-scss"]);
+});
+
 gulp.task("build", ["compile-ts", "compile-scss"]);
-gulp.task("default", ["build"]);
+gulp.task("watch", ["watch-scss"]);
+gulp.task("default", ["build", "watch"]);
