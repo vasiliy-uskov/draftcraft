76f33f9f48a1194072f13f396bc8e13729dd8d57
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-02-21 05:53:27 +0300

    Поправил замечания компилятора

--- src/external/drawablearea/ToolsCreator.ts
+++ src/external/drawablearea/ToolsCreator.ts
@@ -1,6 +1,6 @@
-import {IToolsCreator} from "../../_common/components/workspace/tools/IToolsCreator";
 import {BaseTool} from "../../_common/components/workspace/tools/BaseTool";
 import {ToolFactory} from "../../_common/components/workspace/tools/ToolFactory";
+import {IToolsCreator} from "../../_common/components/workspace/tools/IToolsCreator";
 
 class ToolsCreator implements IToolsCreator {
     createTools(toolFactory: ToolFactory): Array<BaseTool> {
