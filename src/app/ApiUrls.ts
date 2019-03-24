61f0666538fa05853cfa119a8ee84446af026a0e
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-03-24 19:48:24 +0300

    Поправил роутинги сервера

--- src/app/ApiUrls.ts
+++ src/app/ApiUrls.ts
@@ -1,6 +1,6 @@
 enum ApiUrls {
-	getLevels = "./get_levels_ajax",
-	addAnswer = "./add_answer_to_level_ajax",
+	getLevels = "./get_levels",
+	addAnswer = "./add_answer_to_level",
 }
 
 export {ApiUrls};
