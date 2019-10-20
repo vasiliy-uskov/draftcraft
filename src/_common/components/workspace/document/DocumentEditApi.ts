8183e58e2c8a633352db4efc074112f0a91ba1f3
Vasiliy Uskov <vasiliy.uskov@ispringsolution.com>
2019-10-20 20:28:42 +0300

    Избавился от лишнего промиса

--- src/_common/components/workspace/document/DocumentEditApi.ts
+++ src/_common/components/workspace/document/DocumentEditApi.ts
@@ -4,68 +4,66 @@ import {IDocumentEditApi} from "./IWorkspaceModel";
 import {IDocumentView} from "./view/IDocumentView";
 
 class DocumentEditApi implements IDocumentEditApi {
-    constructor(document: Document, documentView: IDocumentView, resolve: (document: Document) => void, reset: () => void) {
-        this._reset = reset;
-        this._resolve = resolve;
-        this._document = document;
-        this._documentView = documentView;
-    }
+	constructor(document: Document, documentView: IDocumentView, commit: (document: Document) => void) {
+		this._commit = commit;
+		this._document = document;
+		this._documentView = documentView;
+	}
 
-    public addDraft(draft: Draft): DocumentEditApi {
-        this._document.draft = this._document.draft.add(draft);
-        return this;
-    }
 
-    public removeDraft(draft: Draft): DocumentEditApi {
-        this._document.draft = this._document.draft.remove(draft);
-        this._document.selection = this._document.selection.remove(draft);
-        return this;
-    }
+	public addDraft(draft: Draft): DocumentEditApi {
+		this._document.draft = this._document.draft.add(draft);
+		return this;
+	}
 
-    public addSelection(draft: Draft): DocumentEditApi {
-        this._document.selection = this._document.selection.add(draft);
-        return this;
-    }
+	public removeDraft(draft: Draft): DocumentEditApi {
+		this._document.draft = this._document.draft.remove(draft);
+		this._document.selection = this._document.selection.remove(draft);
+		return this;
+	}
 
-    public removeSelection(draft: Draft): DocumentEditApi {
-        this._document.selection = this._document.selection.remove(draft);
-        return this;
-    }
+	public addSelection(draft: Draft): DocumentEditApi {
+		this._document.selection = this._document.selection.add(draft);
+		return this;
+	}
 
-    public updateView(): DocumentEditApi {
-        this._documentView.updateState(this._document);
-        return this;
-    }
+	public removeSelection(draft: Draft): DocumentEditApi {
+		this._document.selection = this._document.selection.remove(draft);
+		return this;
+	}
 
-    public draft(): Draft {
-        return this._document.draft
-    }
+	public updateView(): DocumentEditApi {
+		this._documentView.updateState(this._document);
+		return this;
+	}
 
-    public selection(): Draft {
-        return this._document.selection;
-    }
+	public draft(): Draft {
+		return this._document.draft
+	}
 
-    public commit() {
-        if (this._finished) {
-            return;
-        }
-        this._resolve(this._document);
-        this._finished = true;
-    }
+	public selection(): Draft {
+		return this._document.selection;
+	}
 
-    public reset() {
-        if (this._finished) {
-            return;
-        }
-        this._finished = true;
-        this._reset();
-    }
+	public commit() {
+		if (this._finished) {
+			return;
+		}
+		this._commit(this._document);
+		this._finished = true;
+	}
 
-    private _documentView: IDocumentView;
-    private _document: Document;
-    private _resolve: (document: Document) => void;
-    private _reset: () => void;
-    private _finished = false;
+	public reset() {
+		if (this._finished) {
+			return;
+		}
+		this._finished = true;
+	}
+
+	private _documentView: IDocumentView;
+	private _document: Document;
+	private _commit: (document: Document) => void;
+	private _finished = false;
 }
 
 export {DocumentEditApi};
